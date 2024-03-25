4e8a864286efb6daf7e8bbf05fa63720c7354e31
mickael_belleuvre <mickael_belleuvre@connect-tech.sncf>
2024-03-25 16:48:27 +0100

    clean List

--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -1,28 +1,14 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import PokemonCard from '../components/pokemon-card';
 import usePokemons from '../hooks/pokemons.hook';
 import { Link } from 'react-router-dom';
 import PokemonSearch from '../components/pokemon-search';
-import PokemonService from '../services/pokemon-service';
-import PokemonApiResponse from '../models/pokemonsApi';
 import Loader from '../components/loader';
 
 const PokemonList: FunctionComponent = () => {
-  // const pokemons = usePokemons();
+  const pokemons = usePokemons();
 
-  const [pokemonResults, setPokemonResults] = useState<PokemonApiResponse | null>(null);
-
-  useEffect(() => {
-    PokemonService.getPokemonsFromApi()
-      .then((data: any) => setPokemonResults(data))
-      .catch(error => console.error('Erreur lors de la récupération des pokemons :', error));
-  }, []);
-
-
-  console.log(pokemonResults);
-  // console.log(pokemons);
-
-  if (!pokemonResults) {
+  if (!pokemons) {
     return <div><Loader /></div>;
   }
 
@@ -32,24 +18,15 @@ const PokemonList: FunctionComponent = () => {
       <div className="container">
         <h1 className="center">Liste des Pokemons</h1>
         <div className="row">
-          <div>
-            <ul>
-              {pokemonResults.results.map(pokemon => (
-                <li key={pokemon.name}>
-                  <strong>Nom:</strong> {pokemon.name} - <strong>URL:</strong> {pokemon.url}
-                </li>
-              ))}
-            </ul>
-          </div>
-          {/* <PokemonSearch />
+          <PokemonSearch />
           {pokemons.map(pokemon => (
             <PokemonCard key={pokemon.id} pokemon={pokemon} />
-          ))} */}
+          ))}
         </div>
-        {/* <Link to={`/pokemons/add`} className="btn-floating btn-large red waves-effect waves-light z-depth-3"
+        <Link to={`/pokemons/add`} className="btn-floating btn-large red waves-effect waves-light z-depth-3"
           style={{ position: 'fixed', bottom: '25px', right: '25px' }}>
           <i className='material-icons'>add</i>
-        </Link> */}
+        </Link>
       </div>
     </div>
   );
